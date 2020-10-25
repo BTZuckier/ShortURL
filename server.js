8614db081fd15bdd479370910a41f08e776d2eb0
Ben Zuckier <bzuckier@mail.yu.edu>
2020-10-25 14:18:32 -0400

    basic webapp

--- server.js
+++ server.js
@@ -1,74 +1,148 @@
 require('dotenv').config();
 const express = require('express');
-const monk = require('monk');
-const nunjucks = require('nunjucks');
 const helmet = require('helmet');
+const nunjucks = require('nunjucks');
+
+const monk = require('monk');
 const db = monk(process.env.MONGO_PASS);
-const yup = require('yup');
-const cors = require('cors');
+
 const shortid = require('shortid');
-db.then(() => {
+const { static } = require('express');
+
+db.then(() => {//connect to db
     console.log(`connected to mongo`);
 });
-const urls = db.get('urls');
-urls.createIndex('short');
-// urls.createIndex('name');
-// urls.insert([{a: 1}, {a:2}]).then(()=> {
-//     console.log(`done`)
-// })
-const input = yup.object().shape({
-    url: yup.string().trim().url().required(),
-});
+const urls = db.get('urls');//make a collection called urls
+urls.createIndex('short');//index by the short id
+urls.createIndex('url');//index by the long url
 
-const app = express();
-// app.use(express.urlencoded({extended: false}));
-app.use(express.json())
-app.use(helmet());
-app.use(cors());
-
-nunjucks.configure('views', {
+//MIDDLEWARES
+const app = express();//server
+app.use(helmet());//some basic security
+app.use(express.json());//allow json rendering
+app.use(express.urlencoded({ extended: false}));//allow urlencoding
+nunjucks.configure('views', {//set the render engine for nunjucks
     autoescape: true,
     express: app
 });
+// app.use(express.static('./public'));
+app.use('/favicon.ico', express.static('./favicon.ico'));
 
-app.get('/', (req, res) => {
-    res.render('index.njk');
-});
+app.get('/', (req, res)=>{//serve index
+    res.render('index.njk')
+})
+
+function home(req, res){//home function for when we have data
+    var short = req.short;
+    res.render('index.njk', {short})//render and pass context 
+}
 
 app.post('/url', async (req, res, next) => {
-    const url = req.body.LongURL;
-    try {
-        await input.validate({
-            url,
-        });
-    } catch (err) {
-        next(err);
-        // res.status(500).json('ruh roh' + url);
+    const url = req.body.LongURL;//get the long url from the POST
+    //TODO: short words instead
+    //TODO: custom short urls
+    let newUrl = null;
+    try { //check if long url is already in db
+        newUrl = await urls.findOne({ url });
+    }catch(err){
+        console.log(err);
+        res.status(500).json('poop');
     }
-    const short = shortid.generate();
-    const shortUrl = {
-        url,
-        short,
-    };
-    const inserted = await urls.insert(shortUrl);
-    res.json(inserted);
-});
-app.use((err, req, res, next) => {
-    res.status(500).json('ruh roh' + url);
-});
+    if(!newUrl){//Long URL didn't exist in db
+        let short = shortid.generate();
+        while(short.match(/-|_/)){//no dashes or underscores
+            short = shortid.generate();//generate a new id
+        }
+        const shortUrl = {//submit these fields into the db
+            url,
+            short,
+            clicks:0,
+        };
+        newUrl = await urls.insert(shortUrl);
+    }
+    req.short = newUrl;//set url data before passing to be rendered
+        // res.json(inserted);
+        // res.redirect('/api/' + short);
+    return next();//render the page with new data
+}, home);
 
-app.get('/:short', async (req, res)=>{
-    const short = req.params.short;
+app.post('/search', async (req, res, next) => {
+    var short = req.body.shortURL;//get the short url from the POST
+    if(!short){
+        return res.redirect('/');
+    }
+    var domain = short.search("bzuckier.com/");//find the domain
+    if(domain >=0 ){//if there is the domain
+        short = short.substring(domain+13);//get rid of domain, just id
+    }
+    try { //get the short url data
+        shortData = await urls.findOne({ short });
+    }catch(err){
+        console.log(err);
+        res.status(500).json('poop');
+    }
+    if(!shortData){//short URL didn't exist in db
+        return res.status(404).json("That is not a valid short URL.");
+    }
+    req.short = shortData;//set url data before passing to be rendered
+    return next();//render the page with new data
+}, home);
+
+// async function shortData(req, res){
+//     const short = req.params.short;
+//     var shortened = null;
+//     try {
+//         shortened = await urls.findOne({ short });
+//         // if (shortened) {
+//         //     // res.redirect(url.url);
+//         //     res.render('index.njk', { shortened: shortened });
+//         //     // res.redirect('/');
+//         // } else { 
+//         //     res.status(500).json('poo');
+//         // }
+//         // // res.redirect('/');
+//     } catch(err){
+//         console.log(err);
+//         res.status(500).json('poop');
+//     }
+//     return next();
+// }
+
+// app.get('/api/:short', async (req, res) => {
+//     const short = req.params.short;
+//     var shortened = null;
+//     try {
+//         shortened = await urls.findOne({ short });
+//         if (shortened) {
+//             // res.redirect(url.url);
+//             res.render('index.njk', { shortened: shortened });
+//             // res.redirect('/');
+//         } else { 
+//             res.status(500).json('poo');
+//         }
+//         // res.redirect('/');
+//     } catch(err){
+//         console.log(err);
+//         res.status(500).json('poop');
+//     }
+    
+// });
+
+app.get('/:short', async (req, res)=>{//url encoded param, will accept anything matching the pattern
+    const short = req.params.short;//get the short id from the GET
     try {
-        const url = await urls.findOne({ short });
+        const url = await urls.findOne({ short });//find in db
         if (url) {
-            res.redirect(url.url);
+            await urls.update({id: url.id}, {$set: {clicks: url.clicks+1}});//update num clicks
+            res.redirect(url.url);//send to the long url value
         } else { 
-            res.status(500).json('poo');
+            res.status(404).json("That is not a valid short URL.");
         }
     } catch(err){
+        console.log(err);
         res.status(500).json('poo');
     }
 });
 
-app.listen(1337, () => console.log(`Started on port 1337`));
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
